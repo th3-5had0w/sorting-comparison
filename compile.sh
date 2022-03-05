18bcbd84b4c3d7c1b09904e0257ffa83720fd622
th3-5had0w <46859830+th3-5had0w@users.noreply.github.com>
2022-03-05 08:47:51 +0700

    Create compile.sh

---
+++ compile.sh
@@ -0,0 +1,4 @@
+g++ ./heapsort/heapsort.cpp -no-pie -o ./heapsort/heapsort
+g++ ./mergesort/mergesort.cpp -no-pie -o ./mergesort/mergesort
+g++ ./quicksort/quicksort.cpp -no-pie -o ./quicksort/quicksort
+g++ ./cppsortlib/cppsortlib.cpp -no-pie -o ./cppsortlib/cppsortlib
